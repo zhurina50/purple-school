6cac178a899204eb153c370838a19597bc43a74e
zhurina50 <zhurina50@gmail.com>
2023-08-17 20:06:16 -0600

    5-languages

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('дратути');
+let language = prompt('Enter your language');
+switch (language) {
+    case 'russian':
+        console.log('Привет!');
+    break;
+    case 'english':
+        console.log('Hello!');
+    break;
+    case 'spanish':
+        console.log('Hola!');
+    break;
+    case 'german':
+        console.log('Gutten tag!');
+    break;
+    default:
+        console.log('Моя твоя не понимайт!');
+}
