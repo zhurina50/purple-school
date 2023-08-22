35bb435240322b89c6abf375e69ec1937a11ca10
zhurina50 <zhurina50@gmail.com>
2023-08-21 20:17:56 -0600

    7-converter

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
-let language = prompt('Enter your language');
-switch (language) {
-    case 'russian':
-        console.log('Привет!');
-    break;
-    case 'english':
-        console.log('Hello!');
-    break;
-    case 'spanish':
-        console.log('Hola!');
-    break;
-    case 'german':
-        console.log('Gutten tag!');
-    break;
-    default:
-        console.log('Моя твоя не понимайт!');
-}
+function toDollar(sum, currencyOfFunds) {
+    let result;
+    switch (currencyOfFunds) {
+        case 'rubles' :
+           result = sum * 0.01;
+            break;
+        case 'euro' :
+           result = sum * 1.1;
+            break;
+        case 'pesos' :
+            result = sum * 0.06;
+            break;
+        case 'dollars':
+            result = sum * 1;
+            break;
+        default:
+            result = null;
+    }
+    return result;
+};
+
+function calculator(sum, currencyOfFunds, targetCurrency) {
+    let result;
+    switch (targetCurrency) {
+        case 'rubles' :
+           result = toDollar(sum, currencyOfFunds) * 100;
+            break;
+        case 'euro' :
+           result = toDollar(sum, currencyOfFunds) * 0.92;
+            break;
+        case 'pesos' :
+            result = toDollar(sum, currencyOfFunds) * 17;
+            break;
+        case 'dollars':
+            result = toDollar(sum, currencyOfFunds) * 1;
+            break;
+        default:
+            result = null;
+    }
+    return result;
+};
+console.log(calculator(100, 'pesos', 'euro'));
