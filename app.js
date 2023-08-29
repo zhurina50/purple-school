fd5cf021e507b52922d9b74c78e59501c9ee347c
zhurina50 <zhurina50@gmail.com>
2023-08-28 23:25:08 -0600

    9-sort-loops

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-let language = prompt('Enter your language');
-switch (language) {
-    case 'russian':
-        console.log('Привет!');
-    break;
-    case 'english':
-        console.log('Hello!');
-    break;
-    case 'spanish':
-        console.log('Hola!');
-    break;
-    case 'german':
-        console.log('Gutten tag!');
-    break;
-    default:
-        console.log('Моя твоя не понимайт!');
+
+function sort(arr) {
+    for (let element of arr) {
+        for (let j = 0; j < arr.length-1; j++) {
+            if (arr[j] > arr [j+1]) {
+                let a = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = a;
+            }
+        }
+    }
+    return arr;
 }
+console.log(sort([1, 1, 40, -5, 10, 0]));
