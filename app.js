06aab0d2dc67b39a159477c666cc0c09e0bf076d
zhurina50 <zhurina50@gmail.com>
2023-08-29 15:18:53 -0600

    fix problems

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-
+const task = [1, 1, 40, -5, 10, 0];
 function sort(arr) {
-    for (let element of arr) {
+    for (let i = 0; i < arr.length-1; i++) {
         for (let j = 0; j < arr.length-1; j++) {
             if (arr[j] > arr [j+1]) {
-                let a = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = a;
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
             }
         }
     }
     return arr;
 }
-console.log(sort([1, 1, 40, -5, 10, 0]));
+console.log(sort(task));
